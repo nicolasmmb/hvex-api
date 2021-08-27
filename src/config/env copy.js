870760a6c01f3e5a687ed5c499a17df12785e714
Nícolas Marques de Moura Barbosa <59735468+nicolasmmb@users.noreply.github.com>
2021-08-27 16:54:05 -0300

    Delete env copy.js

--- src/config/env copy.js
+++
@@ -1,5 +0,0 @@
-exports.environment = {
-    server: { port: process.env.PORT || 3000 },
-    mongoDB: { devConnection: process.env.DB_URL || 'mongodb://root:root@server.local:27017/' },
-    TOKEN_SECRET: 'HVEX_TOKEN_SECRET',
-}
