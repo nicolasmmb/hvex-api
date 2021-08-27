0533d15e71eb0e301a17a71ce238692efb8d274e
Nícolas Marques de Moura Barbosa <59735468+nicolasmmb@users.noreply.github.com>
2021-08-27 17:18:33 -0300

    Update env.js

--- src/config/env.js
+++ src/config/env.js
@@ -3,6 +3,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.environment = void 0;
 exports.environment = {
     server: { port: process.env.PORT || 3000 },
-    mongoDB: { devConnection: process.env.DB_URL || 'mongodb://root:root@server.local:27017/' },
+    mongoDB: { devConnection: process.env.DB_URL || 'Alterar Para Sua String de Conexão' },
     TOKEN_SECRET: 'HVEX_TOKEN_SECRET',
 }
