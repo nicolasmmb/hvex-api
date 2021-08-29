c7361c2c87693bc3492ef8dcb9e413fed86b43db
nicolas.mmb <nicolas.mmb@hotmail.com>
2021-08-29 14:41:19 -0300

    ...

--- app/server.js
+++ app/server.js
@@ -15,12 +15,12 @@ const app = express();
 app.use(express.json());
 app.use(routes);
 app.use(cors());
-// app.use((req, res, next) => {
-//     res.header('Access-Control-Allow-Origin', '*');
-//     res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE,PATCH');
-//     res.header("Access-Control-Allow-Headers", 'Origin, X-Requested-With, Content-Type, Accept');
-//     next();
-// });
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE,PATCH');
+    res.header("Access-Control-Allow-Headers", 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
 
 
 // Connect to MongoDB
