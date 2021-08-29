f992df8382924e264f81fb86e9c7fa5e4aa2aa63
nicolas.mmb <nicolas.mmb@hotmail.com>
2021-08-29 14:46:23 -0300

    ...

--- app/server.js
+++ app/server.js
@@ -14,11 +14,12 @@ const app = express();
 
 app.use(express.json());
 app.use(routes);
-app.use(cors());
+;
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
-    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE,PATCH');
-    res.header("Access-Control-Allow-Headers", 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header("Access-Control-Allow-Methods", '*');
+    res.header("Access-Control-Allow-Headers", '*');
+    app.use(cors())
     next();
 });
 
