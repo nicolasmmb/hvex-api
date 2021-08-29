5bbd1f05b77b4e246cfec7b1d7aa85bccce01ff1
nicolas.mmb <nicolas.mmb@hotmail.com>
2021-08-29 14:58:16 -0300

    CORS!!!

--- app/server.js
+++ app/server.js
@@ -16,12 +16,12 @@ app.use(express.json());
 app.use(routes);
 app.use(cors())
 app.options('*', cors());
-//app.use((req, res, next) => {
-//    res.header('Access-Control-Allow-Origin', '*');
-//    res.header("Access-Control-Allow-Methods", '*');
-//    res.header("Access-Control-Allow-Headers", '*');
-//    next();
-//});
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header("Access-Control-Allow-Methods", '*');
+    res.header("Access-Control-Allow-Headers", '*');
+    next();
+});
 
 
 // Connect to MongoDB
